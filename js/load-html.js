2ccccee07c0f1f88188fe38a06719a04cf4d8a84
huachao1001 <502591238@qq.com>
2016-06-29 15:03:42 +0800

    动态生成图片+调整markdown显示格式

--- js/load-html.js
+++ js/load-html.js
@@ -24,8 +24,10 @@ function queryArticle(curCategoryId,curPageNo){
 				var authorUrl=object.get("authorUrl");
 				if(!authorUrl){
 					authorUrl="";
-				}
-				
+				} 
+				if(!imgUrl){
+					imgUrl=genImg(title);
+				} 
 				var authorName=object.get("authorName");
 				var tmpItem=oneItem.replace("${arcitleId}",object.id).replace("${title}",title).replace("${imgUrl}",imgUrl).replace("${descript}",descript).replace("${dateTime}",object.createdAt);
 				tmpItem=tmpItem.replace("${category}",_CATEGORY_ID_[categoryId]).replace("${clickCount}",0).replace("${authorName}",authorName).replace("${authorUrl}",authorUrl);
@@ -75,9 +77,9 @@ function genPageBar(curPageNo,categoryId){
 		var tmp="";
 		var prePageNo= parseInt(curPageNo)-1;
 		if(curPageNo<=1){
-			tmp= "<span class=\"disabled\" >first</span><span class=\"disabled\">pre</span>";
+			tmp= "<span class=\"disabled\" >&lt;&lt;</span><span class=\"disabled\">&lt;</span>";
 		}else{ 
-			tmp= "<a  href=\"index.html?c="+categoryId+"&pn=1\"  >first</a><a href=\"index.html?c="+categoryId+"&pn="+prePageNo+"\" >pre</a>";
+			tmp= "<a  href=\"index.html?c="+categoryId+"&pn=1\"  >&lt;&lt;</a><a href=\"index.html?c="+categoryId+"&pn="+prePageNo+"\" >&lt;</a>";
 		}
 		
 		html=html+tmp;
@@ -93,9 +95,9 @@ function genPageBar(curPageNo,categoryId){
 		 
 		var nextPageNo=1+parseInt(curPageNo);
 		if(curPageNo<totalPage){
-			tmp="<a href=\"index.html?c="+categoryId+"&pn="+nextPageNo+"\"  >next</a><a href=\"index.html?c="+categoryId+"&pn="+(totalPage)+"\">last</a>";
+			tmp="<a href=\"index.html?c="+categoryId+"&pn="+nextPageNo+"\"  >&gt;</a><a href=\"index.html?c="+categoryId+"&pn="+(totalPage)+"\">&gt;&gt;</a>";
 		}else{
-			tmp= "<span  class=\"disabled\"  >next</span><span  class=\"disabled\" >last</span>";
+			tmp= "<span  class=\"disabled\"  >&gt;</span><span  class=\"disabled\" >&gt;&gt;</span>";
 		}
 		
 		html=html+tmp;
@@ -106,3 +108,38 @@ function genPageBar(curPageNo,categoryId){
 	  }
 	});
 }
+var hcColors=[ "#6699ff","#99cc33","#ffcc00","#00cc00","#ff9900","#3366cc","#ff6600","#cc0000"];
+function genImg(title){
+	var canvas = document.createElement('canvas');
+	
+	canvas.width = 220;
+	canvas.height = 144; 
+	var color=hcColors[getRandomNum(0,hcColors.length-1)];
+	var ctx=canvas.getContext('2d');
+	var color=hcColors[getRandomNum(0,hcColors.length-1)];
+    //获取对应的CanvasRenderingContext2D对象(画笔)
+    var ctx = canvas.getContext("2d");
+    ctx.fillRect(0,0,220,144);
+    //设置字体样式
+    ctx.font = "bold 20px Courier New";
+	ctx.fillStyle=color;
+ 	ctx.fillRect(0,0,220,144);
+    //设置字体填充颜色
+    ctx.fillStyle = "#ffffff"; 
+	ctx.textAlign='center' 
+	
+	//var width=ctx.measureText(title).width;
+	 
+	ctx.fillText(title, 110, 72);
+	 
+	
+    
+	return canvas.toDataURL("image/png"); 
+}
+
+function getRandomNum(Min,Max)
+{   
+	var Range = Max - Min;   
+	var Rand = Math.random();   
+	return(Min + Math.round(Rand * Range));   
+}  
